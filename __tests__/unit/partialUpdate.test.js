85da93770d27edd9c5e4c14b0c9d38ab1fd2af2a
Muhammed Kilicarslan <muhammed.kilicarslan@ontario.ca>
2020-10-24 19:49:05 -0400

    Add unit test for partialUpdate function

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,21 @@
+const partialUpdate = require("../../helpers/partialUpdate");
+
 describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
+	const logo_url = "https://www.image-url.com";
+	const description = "New Description";
+	const handle = "my-company";
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
+	it("should generate a proper partial update query with just 1 field", function () {
+		const { query, values } = partialUpdate("company", { description }, "handle", handle);
 
-  });
+		expect(query).toEqual("UPDATE company SET description=$1 WHERE handle=$2 RETURNING *");
+		expect(values).toEqual([description, handle]);
+	});
+
+	it("should generate a proper partial update query with multiple fields", function () {
+		const { query, values } = partialUpdate("company", { description, logo_url }, "handle", handle);
+
+		expect(query).toEqual("UPDATE company SET description=$1, logo_url=$2 WHERE handle=$3 RETURNING *");
+		expect(values).toEqual([description, logo_url, handle]);
+	});
 });
